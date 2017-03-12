9dd4691530a8784e3b344813e48e5517695aae16
mhostetter <mhostetter@users.noreply.github.com>
2017-03-12 10:16:32 -0400

    Implemented basic SQLite Triggered Source block functionality.

--- python/triggered_source.py
+++ python/triggered_source.py
@@ -19,24 +19,56 @@
 # Boston, MA 02110-1301, USA.
 # 
 
-import numpy
+import numpy as np
 from gnuradio import gr
+import pmt
+import sqlite3
 
 class triggered_source(gr.sync_block):
     """
     docstring for block triggered_source
     """
-    def __init__(self, filename, table_name, vector_column_name):
+    def __init__(self, filename, table_name, vector_column_name, sql_condition):
         gr.sync_block.__init__(self,
             name="triggered_source",
-            in_sig=[<+numpy.float+>],
-            out_sig=[<+numpy.float+>])
+            in_sig=None,
+            out_sig=None)
+
+        self.table_name = table_name
+        self.vector_column_name = vector_column_name
+        self.sql_condition = sql_condition
+
+        # Establish database connection
+        self.conn = sqlite3.connect(filename, check_same_thread=False)
+        self.c = self.conn.cursor()
+        self.conn.text_factory = str
+
+        # Start SQL query and then fetch one row per trigger
+        self.c.execute('SELECT * FROM ' + self.table_name + ' ' + self.sql_condition)
+
+        self.message_port_register_in(pmt.string_to_symbol('trigger'))
+        self.message_port_register_out(pmt.string_to_symbol('pdu'))
+        self.set_msg_handler(pmt.string_to_symbol('trigger'), self.fetch_new_pdu)
+
+
+    def fetch_new_pdu(self, trigger):
+        # Fetch the next row of the database
+        row = self.c.fetchone()
+
+        if row is not None:
+            # Format into PDU
+            meta = dict()
+            vector = None
+            for idx, col in enumerate(self.c.description):
+                column = col[0]
+                if column == self.vector_column_name:
+                    vector = pmt.to_python(pmt.deserialize_str(str(row[idx])))
+                else:
+                    meta[col[0]] = row[idx]
+            
+            pdu = pmt.cons(pmt.to_pmt(meta), pmt.to_pmt(vector))
+            self.message_port_pub(pmt.string_to_symbol('pdu'), pdu)
 
 
     def work(self, input_items, output_items):
-        in0 = input_items[0]
-        out = output_items[0]
-        # <+signal processing here+>
-        out[:] = in0
-        return len(output_items[0])
-
+        assert(False)
